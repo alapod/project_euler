2471bcdb37a303a107407ea290fdbd6327e407d8
alapod <alapodrezova@gmail.com>
2022-12-28 22:06:33 +0500

    refactor solution

--- task4.py
+++ task4.py
@@ -1,16 +1,11 @@
-import math
-
-
 def is_palindrome(n):
     return str(n) == str(n)[::-1]
 
 
 def find_largest_palindrome():
     palindromes = []
-    for i in range(999, 100, -1):
-       # print(i)
-        for j in range(i, 100, -1):
-         #   print(i, j)
+    for i in range(100, 999):
+        for j in range(i, 999):
             if is_palindrome(i * j):
                 palindromes.append(i * j)
     return sorted(palindromes)[-1]
