6042ef10d0712c68703fed0b4a4d29273ce68976
alapod <alapodrezova@gmail.com>
2022-12-29 20:43:55 +0500

    solved task 7

---
+++ task7.py
@@ -0,0 +1,19 @@
+from math import log, sqrt, ceil
+
+
+def estimate(n):
+    return n * log(n) + n * log(log(n))
+
+
+def find_primes(n):
+    nums = [True] * (n+1)
+    for i in range(2, round(sqrt(n))):
+        if nums[i]:
+            for j in range(i ** 2, n+1, i):
+                nums[j] = False
+    primes = [index for index, item in enumerate(nums) if item]
+    return primes
+
+
+print(find_primes(ceil(estimate(10001)))[10002])
+
